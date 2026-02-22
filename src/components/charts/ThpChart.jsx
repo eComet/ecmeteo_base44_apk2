c618b319afe5b23775e1765864a49fe835f62725
minar.janko <minar.janko@gmail.com>
2026-02-22 23:06:04 +0000

    File changes

--- src/components/charts/ThpChart.jsx
+++ src/components/charts/ThpChart.jsx
@@ -310,7 +310,7 @@ export default function ThpChart({ timestamps, series }) {
                 onMouseUp={handleMouseUp}
                 onMouseLeave={handleMouseUp}
             >
-                <ResponsiveContainer width="100%" height={350}>
+                <ResponsiveContainer width="100%" height={350} className="landscape:h-64">
                     <ComposedChart data={data} margin={{ top: 10, right: 80, left: 10, bottom: 30 }}>
                         <CartesianGrid strokeDasharray="2 2" stroke="#e5e7eb" horizontal={true} vertical={true} />
 
